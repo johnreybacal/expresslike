81a499f8a3957bb660714e084408729a8e1eb141
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-09-14 13:51:26 +0800

    add request method

---
+++ src/main/java/bacal/johnrey/expresslike/http/Method.java
@@ -0,0 +1,29 @@
+package bacal.johnrey.expresslike.http;
+
+/**
+ * Request methods
+ */
+public enum Method {
+    GET("GET"),
+    POST("POST"),
+    PUT("PUT"),
+    PATCH("PATCH"),
+    DELETE("DELETE");
+
+    private final String name;
+
+    private Method(String method) {
+        name = method;
+    }
+
+    public boolean equalsName(String otherName) {
+        // (otherName == null) check is not needed because name.equals(null) returns
+        // false
+        return name.equals(otherName);
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
