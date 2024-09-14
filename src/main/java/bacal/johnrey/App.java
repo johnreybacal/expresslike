9e9a6100d053fe9afcbfd21b8d7f463e36282ef7
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-09-14 15:40:12 +0800

    reuse router on app

--- src/main/java/bacal/johnrey/App.java
+++ src/main/java/bacal/johnrey/App.java
@@ -5,19 +5,47 @@ import java.net.InetSocketAddress;
 
 import com.sun.net.httpserver.HttpServer;
 
-public class App {
+import bacal.johnrey.expresslike.http.Method;
+import bacal.johnrey.expresslike.http.Middleware;
+import bacal.johnrey.expresslike.route.Handler;
+import bacal.johnrey.expresslike.route.Registry;
+import bacal.johnrey.expresslike.route.Route;
+import bacal.johnrey.expresslike.route.Router;
+
+public class App extends Router {
     private HttpServer server;
+    private Registry registry;
 
     public App() throws IOException {
-        this.server = HttpServer.create();
+        super("");
+        server = HttpServer.create();
+        registry = new Registry();
+
+        Handler handler = new Handler(registry);
+        server.createContext("/", handler);
     }
 
     public void listen(int port, int backlog) throws IOException {
-        this.server.bind(new InetSocketAddress(port), backlog);
+        server.bind(new InetSocketAddress(port), backlog);
+        server.start();
     }
 
     public void listen(int port) throws IOException {
         listen(port, 0);
+    }
+
+    public void use(Route route) {
+        registry.register(route);
+    }
+
+    public void use(Router router) {
+        registry.register(router.getRoutes().toArray(new Route[0]));
+    }
+
+    @Override
+    protected void on(Method method, String url, Middleware... middlewares) {
+        Route route = new Route(url, method, middlewares);
+        this.registry.register(route);
     }
 
 }
