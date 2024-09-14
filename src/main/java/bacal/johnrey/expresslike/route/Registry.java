3ced072fd2453da3cefbdd876296315d2fee64e0
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-09-14 15:39:40 +0800

    make registry instantiatable outside

--- src/main/java/bacal/johnrey/expresslike/route/Registry.java
+++ src/main/java/bacal/johnrey/expresslike/route/Registry.java
@@ -5,23 +5,10 @@ import java.util.List;
 
 public class Registry {
 
-    private static volatile Registry instance;
-
     private final List<Route> routes;
 
-    private Registry() {
+    public Registry() {
         this.routes = new ArrayList<>();
-    }
-
-    public static Registry getInstance() {
-        if (instance != null) {
-            synchronized (Registry.class) {
-                if (instance != null) {
-                    instance = new Registry();
-                }
-            }
-        }
-        return instance;
     }
 
     /**
