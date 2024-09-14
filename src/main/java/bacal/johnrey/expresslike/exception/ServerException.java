3eb36eae350b83116c3fee0c2c795de24d93989b
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-09-14 14:08:10 +0800

    add serverexception

---
+++ src/main/java/bacal/johnrey/expresslike/exception/ServerException.java
@@ -0,0 +1,28 @@
+package bacal.johnrey.expresslike.exception;
+
+/**
+ * Exception with status code
+ */
+public class ServerException extends Exception {
+
+    final private int statusCode;
+
+    public ServerException(Throwable e) {
+        super(e);
+        this.statusCode = 500;
+    }
+
+    public ServerException(int statusCode, String message) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public static ServerException notFound() {
+        return new ServerException(404, "Resource not found.");
+    }
+
+}
