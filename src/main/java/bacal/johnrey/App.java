56a70f34466e327f0c00ae9dca65d8803bc75d47
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-09-14 15:09:51 +0800

    setup app

--- src/main/java/bacal/johnrey/App.java
+++ src/main/java/bacal/johnrey/App.java
@@ -1,13 +1,23 @@
 package bacal.johnrey;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+
+public class App {
+    private HttpServer server;
+
+    public App() throws IOException {
+        this.server = HttpServer.create();
     }
+
+    public void listen(int port, int backlog) throws IOException {
+        this.server.bind(new InetSocketAddress(port), backlog);
+    }
+
+    public void listen(int port) throws IOException {
+        listen(port, 0);
+    }
+
 }
